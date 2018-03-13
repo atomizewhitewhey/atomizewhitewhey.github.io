fe21de2d26f7bb7c46caa6571db9221022129d07
= <lim.weiheng.hadrian@gmail.com>
2018-03-13 16:46:25 +0000

    Added article navbar transition

--- scripts/article-header.js
+++ scripts/article-header.js
@@ -1,18 +1,20 @@
 var lastScrollTop = window.pageYOffset;
+var header = document.querySelector(".article-header");
+var navbar = document.querySelector("#article-nav");
 
 function displayHeader() {
   var newY = window.pageYOffset;
-
-  var header = document.querySelector(".article-header");
 
   if (newY > lastScrollTop) {
     lastScrollTop = newY;
 
     header.classList.add("hide-header");
+    navbar.classList.remove("shift-down-for-header");
     return;
   }
 
   header.classList.remove("hide-header");
+  navbar.classList.add("shift-down-for-header");
   lastScrollTop = newY;
 }
 
